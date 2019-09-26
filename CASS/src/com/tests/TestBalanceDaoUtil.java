dc8399e85e1ca01d3e32c3fd29a3288d27915d17
Nk <Nk@Nks.localdomain>
2019-09-26 11:19:21 +0530

    Test Package added!

---
+++ CASS/src/com/tests/TestBalanceDaoUtil.java
@@ -0,0 +1,27 @@
+package com.tests;
+
+import org.junit.Test;
+
+public class TestBalanceDaoUtil {
+
+	@Test
+	public void testGetAllBalances() {
+	}
+
+	@Test
+	public void testUpdateAllBalances() {
+	}
+
+	@Test
+	public void testGetBalanceByClearingMember() {
+	}
+
+	@Test
+	public void testUpdateSecurity() {
+	}
+
+	@Test
+	public void testUpdateFunds() {
+	}
+
+}
